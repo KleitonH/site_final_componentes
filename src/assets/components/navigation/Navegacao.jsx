be579383830a225cb5b0ad736b4d403c8c1d5184
machadocaetano <machadocaetano@rede.ulbra.br>
2024-12-05 23:52:55 -0300

    Tela de mostrar produtos cadastrados

--- src/assets/components/navigation/Navegacao.jsx
+++ src/assets/components/navigation/Navegacao.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function Navegacao(){
     return <>
         <nav className="navigation" style={{display: 'flex', justifyContent:'space-around'}}>
-                <NavLink className={({isActive}) => isActive ? "active" : ""}  to={"/home"}>Home</NavLink>
+                <NavLink className={({isActive}) => isActive ? "active" : ""}  to={"/home"}>Início</NavLink>
                 <NavLink to={"/produtos"}>Produtos</NavLink>
                 <NavLink to={"/cadastrar"}>Cadastrar Produto</NavLink>
         </nav>
--- src/pages/ProdutoPage.jsx
+++ src/pages/ProdutoPage.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
+
 function ProdutoPage(){
-    
-    const [produtos, setProdutos] = useState([]);
+
+    const [produtos, setProdutos] = useState([])
+
+    async function pegarProdutos(){
+        const listaProdutos = await fetch("http://localhost:3001/produtos")
+        const lista = await listaProdutos.json()
+
+        setProdutos(lista)
+    }
+
+    useEffect(() => {
+        pegarProdutos()
+    }, [])
+
+    return(
+        <div style={{display:"flex", flexWrap:"wrap", gap:"20px", justifyContent:"space-around"}}>
+            {produtos.map(item => {
+                return <div>
+                    <h2>{item.descricao}</h2>
+                    <h3>{item.quantidade}</h3>
+                    <h3>{item.preco}</h3>
+                    <img src={item.imagem} alt="imagem produto" />
+                </div>
+            })}
+        </div>
+    )
 
     
 }
